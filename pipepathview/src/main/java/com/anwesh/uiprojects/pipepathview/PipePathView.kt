c2e7620aa29564a5ba2ab564cc7846fb13930695
Anwesh43 <anweshthecool0@gmail.com>
2018-09-02 01:01:57 +0530

    adding libraries and defining nodes

--- pipepathview/src/main/java/com/anwesh/uiprojects/pipepathview/PipePathView.kt
+++ pipepathview/src/main/java/com/anwesh/uiprojects/pipepathview/PipePathView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.pipepathview
 /**
  * Created by anweshmishra on 02/09/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val nodes : Int = 5
+
+
