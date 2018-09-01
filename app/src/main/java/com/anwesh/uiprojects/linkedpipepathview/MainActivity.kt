fe9cd84ac65984cc9c0cf4f5d7a47042c57ed6c3
Anwesh43 <anweshthecool0@gmail.com>
2018-09-02 01:40:30 +0530

    adding PipePathView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedpipepathview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedpipepathview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedpipepathview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.pipepathview.PipePathView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        PipePathView.create(this)
     }
 }
